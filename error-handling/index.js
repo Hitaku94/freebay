a07d9881d9a2ca9f9f17fb4fed93533d61a73449
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-29 23:57:51 +0200

    testing again

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,11 +1,12 @@
 module.exports = (app) => {
-  const imgPic = req.session.useInfo.img
   app.use((req, res, next) => {
+    let imgPic = req.session.useInfo.img
     // this middleware runs whenever requested page is not available
     res.status(404).render("not-found.hbs", {imgPic});
   });
 
   app.use((err, req, res, next) => {    
+    let imgPic = req.session.useInfo.img
     // whenever you call next(err), this middleware will handle the error
     // always logs the error
     console.error("ERROR", req.method, req.path, err);
