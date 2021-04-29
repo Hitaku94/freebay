e5de74169b0c81d821a60e65c660de71b218d01f
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-29 23:53:10 +0200

    no pic rollback

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,19 +1,18 @@
 module.exports = (app) => {
-  const imgPic = req.session.useInfo.img
+  // const imgPic = req.session.useInfo.img
   app.use((req, res, next) => {
     // this middleware runs whenever requested page is not available
-    res.status(404).render("not-found.hbs", {imgPic});
+    res.status(404).render("not-found.hbs");
   });
 
   app.use((err, req, res, next) => {    
     // whenever you call next(err), this middleware will handle the error
     // always logs the error
-    const imgPic = req.session.userInfo.img
     console.error("ERROR", req.method, req.path, err);
 
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
-      res.status(500).render("error", {imgPic});
+      res.status(500).render("error");
     }
   });
 };
