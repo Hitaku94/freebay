01a2aed780d5df907a3685c22409a3bfe2ef0953
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-29 20:30:29 +0200

    settings and error handling

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,17 +1,18 @@
 module.exports = (app) => {
+  const imgPic = req.session.useInfo.img
   app.use((req, res, next) => {
     // this middleware runs whenever requested page is not available
-    res.status(404).render("not-found.hbs");
+    res.status(404).render("not-found.hbs", {imgPic});
   });
 
-  app.use((err, req, res, next) => {
+  app.use((err, req, res, next) => {    
     // whenever you call next(err), this middleware will handle the error
     // always logs the error
     console.error("ERROR", req.method, req.path, err);
 
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
-      res.status(500).render("error");
+      res.status(500).render("error", {imgPic});
     }
   });
 };
