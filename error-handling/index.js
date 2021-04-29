ebdd7686efc5798a54fcb1ea3e18a15d35f885c3
Takayuki Wada <takayuki.wada@hotmail.fr>
2021-04-29 20:19:08 +0200

    settings settled

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
   app.use((req, res, next) => {
+    
     // this middleware runs whenever requested page is not available
     res.status(404).render("not-found.hbs");
   });
@@ -7,11 +8,12 @@ module.exports = (app) => {
   app.use((err, req, res, next) => {
     // whenever you call next(err), this middleware will handle the error
     // always logs the error
+    const imgPic = req.session.userInfo.img
     console.error("ERROR", req.method, req.path, err);
 
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
-      res.status(500).render("error");
+      res.status(500).render("error", {imgPic});
     }
   });
 };
